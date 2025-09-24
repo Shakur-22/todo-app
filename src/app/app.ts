704ccb4810bcfb71b732c63a61d7dcd031cb919a
Shakur-22 <jordan.wright2222@yahoo.com>
2025-09-24 16:57:28 -0400

    added the edit function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 // app.component.ts
 import { Component, input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,17 +17,60 @@ selectedcatagory = 'In Progress'
 
 newtask = ''
 
+etask = ''
+
+
 tasks = [
-  {text: 'Task 1', completed: false},
-  {text: 'Task 2', completed: false},
-  {text: 'Task 3', completed: false}
+  {text: 'Buy groceries', completed: false, edit: false},
+  {text: 'Schedule dentist appointment', completed: false, edit: false},
+  {text: 'Complete project proposal', completed: false, edit: false},
+  {text: 'Call mom', completed: false, edit: false},
+  {text: 'Pay electricity bill', completed: false, edit: false},
+  {text: 'Walk the dog', completed: false, edit: false},
+  {text: 'Read 20 pages of book', completed: false, edit: false},
+  {text: 'Clean bathroom', completed: false, edit: false},
+  {text: 'Submit timesheet', completed: false, edit: false},
+  {text: 'Water plants', completed: false, edit: false},
+  {text: 'Update resume', completed: false, edit: false},
+  {text: 'Organize desk workspace', completed: false, edit: false},
+  {text: 'Book vacation flights', completed: false, edit: false},
+  {text: 'Fix leaky faucet', completed: false, edit: false},
+  {text: 'Attend team meeting', completed: false, edit: false},
+  {text: 'Take car for oil change', completed: false, edit: false},
+  {text: 'Prep for presentation', completed: false, edit: false},
+  {text: 'Return library books', completed: false, edit: false},
+  {text: 'Exercise for 30 minutes', completed: false, edit: false},
+  {text: 'Research new laptop', completed: false, edit: false},
+  {text: 'Plan weekend trip', completed: false, edit: false},
+  {text: 'Send birthday card to aunt', completed: false, edit: false},
+  {text: 'Backup computer files', completed: false, edit: false},
+  {text: 'Review monthly budget', completed: false, edit: false},
+  {text: 'Clean out email inbox', completed: false, edit: false},
+  {text: 'Pick up dry cleaning', completed: false, edit: false},
+  {text: 'Install new software updates', completed: false, edit: false},
+  {text: 'Practice guitar for 20 minutes', completed: false, edit: false},
+  {text: 'Meal prep for next week', completed: false, edit: false},
+  {text: 'Write thank you notes', completed: false, edit: false}
 ]
+edittask(taskToEdit: any) {
+  const index = this.tasks.indexOf(taskToEdit);
+  this.tasks[index].edit = true;
+}
 
-
+updateTask(taskToUpdate: any) {
+  const index = this.tasks.indexOf(taskToUpdate);
+  this.tasks[index].text = this.etask;
+  this.tasks[index].edit = false
+  this.etask = ''
+}
 
 addtask() {
-  this.tasks.push({text: this.newtask, completed: false})
+  if (this.newtask != ''){
+  this.tasks.push({text: this.newtask, completed: false, edit: false})
   this.newtask = ''
+  } else {
+    alert("Enter Task")
+  }
 }
 
 countComplete() {
